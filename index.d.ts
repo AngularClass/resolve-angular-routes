1aea65bc6a452cd362d272a86ca20ec687583603
PatrickJS <github@gdi2290.com>
2016-08-01 02:31:29 -0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,1 @@
-export function resolveNgRoute(srcPath: string, config?: any, defaultFile?: string, resolver?: any): any;
+export default function resolveNgRoute(srcPath: string, config?: any, defaultFile?: string, resolver?: any): any;
