2aca2acd5f3d9a70b012788c03eb92040b87d518
PatrickJS <github@gdi2290.com>
2016-08-01 02:24:22 -0700

    feat: src

---
+++ index.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+var path = require('path');
+
+function routesResolver(filePath) {
+  var ngModule = require(filePath).default || require(filePath);
+  if (ngModule.routes && ngModule.routes.length) {
+    return ngModule.routes;
+  }
+  return [];
+}
+
+function resolveNgRoute(srcPath, config, defaultFile, resolver) {
+  config = config || {};
+  resolver = resolver || routesResolver;
+  defaultFile = defaultFile || 'index.ts';
+  fs.readdirSync(srcPath)
+    .filter(function(file) {
+      var filePath = path.join(srcPath, file);
+      return fs.statSync(filePath).isDirectory() || file === defaultFile;
+    })
+    .map(function(file) {
+      var filePath = path.join(srcPath, file);
+      if (fs.statSync(filePath).isDirectory()) {
+        return getDirectories(filePath, config, defaultFile, resolver);
+      }
+      resolver(filePath)
+        .filter(function(route) {
+          return route.component && typeof route.component === 'string';
+        })
+        .map(function(route) { return route.component.split('#')[0] })
+        .map(function(componentPath) {
+          //  create context map
+          config[componentPath] = path.join(srcPath, componentPath);
+        });
+      // return filePath;
+    });
+  return config;
+}
+resolveNgRoute.default = resolveNgRoute
+
+module.exports = resolveNgRoute;
